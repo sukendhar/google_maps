f137393ebafd10a4472eddfa035ec6873ff49bd6
maisaengineering <contact@maisasolutions.com>
2012-08-24 18:53:27 +0530

    first commit

---
+++ app/models/character.rb
@@ -0,0 +1,9 @@
+class Character < ActiveRecord::Base
+  attr_accessible :address, :gmaps, :latitude, :longitude, :name
+
+  acts_as_gmappable
+
+	def gmaps4rails_address
+	 address 
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Map::Application.config.secret_token = '8dc440950a44eabda9f33b5c5062bafffb4272d167a6c9493c64d6d2e2cbb5f49b4b4e8a97ef8f60d6e9631310967bb9e1abfc8563fc5bbf4077ac03168e389e'
